e61b2c9a99ca8f6d2333a9417c20cd41e22964ec
Ujjawalb01 <ujjawalsharma507@gmail.com>
2025-11-05 00:04:51 +0530

    utils

--- src/index.js
+++ src/index.js
@@ -1,47 +1,17 @@
 import dotenv from "dotenv";
-
+import express from "express"
 import connectDB from "./db/index.js";
-
 
 dotenv.config({
   path: "./env",
 });
-
-connectDB();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-import express from "express";
-const app = express()(async () => {
-  try {
-    await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
-    app.on("error", (error) => {
-      console.log("Error:", error);
-      throw error;
+const app = express()
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`✅ Server is running at PORT : ${process.env.PORT}`);
     });
-
-    app.listen(process.env.PORT, () => {
-      console.log(`App is listening ob port ${process.env.PORT}`);
-    });
-  } catch (error) {
-    console.log(error);
-    throw error;
-  }
-})();
-*/
+  })
+  .catch((err) => {
+    console.log("❌ MOngoDB Connection failed !!", err);
+  });
