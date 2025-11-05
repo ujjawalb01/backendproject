614ae1c69759098dec9363b25c58ce00d7e7217b
Ujjawalb01 <ujjawalsharma507@gmail.com>
2025-11-06 00:03:35 +0530

    done

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-import express from " express";
+import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 const app = express();
@@ -10,8 +10,15 @@ app.use(
   })
 );
 app.use(express.json({ limit: "16kb" }));
-app.use(express.urlencodded({ extended: true, limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
 app.use(cookieParser());
+
+//routes importing
+import userRouter from "./routes/user.routes.js";
+
+// routes declaration
+app.use("/api/v1/user",userRouter)
+
 
 export { app };
--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import dotenv from "dotenv";
 import express from "express"
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
   path: "./env",
 });
-const app = express()
+// const app = express()
 connectDB()
   .then(() => {
     app.listen(process.env.PORT || 8000, () => {
