f2a2645af61b8f51594522f19292cfd303032570
Ujjawalb01 <ujjawalsharma507@gmail.com>
2025-11-07 21:27:25 +0530

    Fix response format in getCurrentUser and ensure await in updateAccountDetails

--- src/controllers/User.controller.js
+++ src/controllers/User.controller.js
@@ -220,7 +220,7 @@ const changeCurrentPassword = asyncHandler(async(req,res)=>{
 const getCurrentUser = asyncHandler(async(req,res) => {
   return res
   .status(200)
-  .json(200,req.user, "Current user fetched successfully")
+  .json(new ApiResponse(200,req.user, "Current user fetched successfully"))
 })
 
 const updateAccountDetails = asyncHandler(async(req,res) => {
@@ -232,7 +232,7 @@ const updateAccountDetails = asyncHandler(async(req,res) => {
     throw new ApiError(400,"All field are required")
   }
 
-  const user = User.findByIdAndUpdate(
+  const user = await User.findByIdAndUpdate(
     req.user?._id,
     {
       $set: {
